96adbe4574f1d40c18c590d19f68eb4559cc366f
Benjamin Dehli <benjamin@arkitektum.no>
2016-08-24 13:37:06 +0200

    Combine multiple small javascript files to single file

---
+++ src/js/helpers.js
@@ -0,0 +1,35 @@
+/* Tabs */
+function activateTab(tab){
+	$(".link-tabs").ready(function () {
+		tabLink = $(".link-tabs li a[data-tab='" + tab + "']");
+		$(".link-tabs li.active").removeClass('active');
+		tabLink.parent('li').addClass('active');
+	});
+}
+
+$(".link-tabs").ready(function () {
+	$(".link-tabs li a").click(function (event){
+		event.preventDefault();
+		activateTab($(this).data('tab'));
+		$("#tab-content").css('opacity', '.15');
+		window.location.href = $(this).prop('href');
+	});
+});
+
+
+/* Help texts */
+$("document").ready( function(){
+	$("a.help-text-toggle").click(function (event) {
+		event.preventDefault();
+		var toggleButton = $(this);
+		var helpTextId = $(this).data("help-text-id");
+		$("#" + helpTextId).toggle();
+		if ($("#" + helpTextId).hasClass('active')) {
+			$("#" + helpTextId).removeClass('active');
+			toggleButton.removeClass('active');
+		} else {
+			$("#" + helpTextId).addClass('active');
+			toggleButton.addClass('active');
+		}
+	});
+});
