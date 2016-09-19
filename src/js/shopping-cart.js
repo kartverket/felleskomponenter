ba75a7384f98c963f6a26e6a48577411810f9fae
Benjamin Dehli <benjamin@arkitektum.no>
2016-09-19 15:20:25 +0200

    Add tooltip for shopping cart

--- src/js/shopping-cart.js
+++ src/js/shopping-cart.js
@@ -1,3 +1,14 @@
+function addShoppingCartTooltip(elementsCount) {
+    var element = $('#shopping-car-url');
+    var elementsCountText = elementsCount !== 0 ? elementsCount : 'ingen';
+    var text = elementsCount == 1 ? 'Du har ' + elementsCountText + ' nedlasting i kurven din' : 'Du har ' + elementsCountText + ' nedlastinger i kurven din';
+    element.attr('title', text);
+    element.attr('data-original-title', text);
+    element.data('toggle', 'tooltip');
+    element.data('placement', 'bottom');
+    element.tooltip();
+}
+
 function updateShoppingCart() {
     var shoppingCartElement = $('#orderitem-count');
     var orderItems = "";
@@ -15,12 +26,15 @@ function updateShoppingCart() {
         orderItemsObj = JSON.parse(orderItems);
         cookieValue = orderItemsObj.length;
         shoppingCartElement.html(cookieValue);
+        addShoppingCartTooltip(cookieValue);
     } else if (Cookies.get(cookieName) !== undefined && Cookies.get(cookieName) !== 0 && Cookies.get(cookieName) !== "0") {
         cookieValue = Cookies.get(cookieName);
         shoppingCartElement.css("display", "block");
         shoppingCartElement.html(cookieValue);
+        addShoppingCartTooltip(cookieValue);
     } else {
         shoppingCartElement.css("display", "none");
+        addShoppingCartTooltip(0);
     }
     Cookies.set(cookieName, cookieValue, { expires: 7, path: '/', domain: cookieDomain });
 }
@@ -35,9 +49,11 @@ function updateShoppingCartCookie() {
         var orderItemsObj = JSON.parse(orderItems);
         cookieValue = orderItemsObj.length;
         shoppingCartElement.html(cookieValue);
+        addShoppingCartTooltip(cookieValue);
     } else {
         cookieValue = 0;
         shoppingCartElement.css("display", "none");
+        addShoppingCartTooltip(cookieValue);
     }
     Cookies.set(cookieName, cookieValue, { expires: 7, path: '/', domain: cookieDomain });
 }
